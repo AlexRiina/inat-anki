30a8571c1deed81bda2ffd591f9f56339aa2a149
Alex Riina <alex.riina@gmail.com>
2022-01-12 19:05:29 -0500

    make options part of the argparse interface and add readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# Inaturalist to Anki scraper
+
+This script is not designed to help you launch your inaturalist competitor:
+it's a simple script for downloading a small amount of animal information and
+adding it to Anki through
+[Anki-Connect](https://github.com/FooSoft/anki-connect/).
+
+## Installation
+
+1. clone this repo
+1. `pip install -r requirements.txt`
+1. Install [Anki-Connect](https://github.com/FooSoft/anki-connect/)
+
+## Generating notes
+
+1. Launch Anki with Anki-Connect running
+1. Find either a `/taxa` or `/observation` page like to download
+1. Run script to automatically create model and notes
+    ```sh
+    python run.py 'https://www.inaturalist.org/observations?iconic_taxa=Fungi&place_id=2&subview=table&view=species'
+    ```
+
+![Anki Add Note dialog with Inaturalistt Model](./note_screenshot.png)
--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 """
-inaturalist scraper to anki scraper
+inaturalist scraper that to writes anki card via anki-connect
 """
 
-import logging
 import argparse
 import json
+import logging
 import re
 from dataclasses import dataclass
 from typing import Iterable, List, Optional
@@ -12,6 +12,7 @@ from urllib.parse import parse_qs, urlparse
 
 import bs4
 import requests
+import tqdm
 
 ANKI_HOST = "http://localhost:8765"
 
@@ -62,8 +63,8 @@ def download_species_list(url) -> Iterable[Taxon]:
     )
     resp.raise_for_status()
     for taxon_data in resp.json()["results"]:
-        if int(taxon_data['count']) < 10:
-            logging.info("Skipping %s", taxon_data['taxon']['name'])
+        if int(taxon_data["count"]) < 10:
+            logging.info("Skipping %s", taxon_data["taxon"]["name"])
             continue
 
         try:
@@ -73,10 +74,68 @@ def download_species_list(url) -> Iterable[Taxon]:
             if taxon:
                 yield taxon
         except Exception:
-            logging.warning("Unable to download %s", taxon_data['taxon']['name'])
+            logging.warning("Unable to download %s", taxon_data["taxon"]["name"])
 
 
-def create_anki_card(taxon: Taxon):
+def create_anki_model(model_name):
+    divider = '\n<hr id="answer">\n'
+
+    resp = requests.post(
+        ANKI_HOST,
+        json={
+            "action": "createModel",
+            "version": 6,
+            "params": {
+                "modelName": "Inaturalist2",
+                "inOrderFields": [
+                    "Name",
+                    "Scientific Name",
+                    "Image 1",
+                    "Image 2",
+                    "Image 3",
+                    "Image 4",
+                ],
+                "isCloze": False,
+                "cardTemplates": [
+                    {
+                        "Name": "Name -> Scientific Name",
+                        "Front": "{{Name}}",
+                        "Back": "{{FrontSide}}" + divider + "{{Scientific Name}}",
+                    },
+                    {
+                        "Name": "Image 1 -> Name",
+                        "Front": "{{#Image 1}}<div style='font-family: \"Arial\"; font-size: 20px;'>{{Image 1}}</div>{{/Image 1}}",
+                        "Back": "{{FrontSide}}" + divider + "{{Name}}",
+                    },
+                    {
+                        "Name": "Image 2 -> Name",
+                        "Front": "{{#Image 2}}<div style='font-family: \"Arial\"; font-size: 20px;'>{{Image 2}}</div>{{/Image 2}}",
+                        "Back": "{{FrontSide}}" + divider + "{{Name}}",
+                    },
+                    {
+                        "Name": "Image 3 -> Name",
+                        "Front": "{{#Image 3}}<div style='font-family: \"Arial\"; font-size: 20px;'>{{Image 3}}</div>{{/Image 3}}",
+                        "Back": "{{FrontSide}}" + divider + "{{Name}}",
+                    },
+                    {
+                        "Name": "Image 4 -> Name",
+                        "Front": "{{#Image 4}}<div style='font-family: \"Arial\"; font-size: 20px;'>{{Image 4}}</div>{{/Image 4}}",
+                        "Back": "{{FrontSide}}" + divider + "{{Name}}",
+                    },
+                ],
+            },
+        },
+    )
+
+    resp.raise_for_status()
+    if error := resp.json()["error"]:
+        if "already exists" not in error:
+            raise Exception(error)
+        else:
+            print("Inaturalist model already exists")
+
+
+def create_anki_card(taxon: Taxon, anki_deck: str, anki_model: str, tags: List[str]):
     resp = requests.post(
         ANKI_HOST,
         json={
@@ -84,8 +143,8 @@ def create_anki_card(taxon: Taxon):
             "version": 6,
             "params": {
                 "note": {
-                    "deckName": "Default",
-                    "modelName": "Inaturalist",
+                    "deckName": anki_deck,
+                    "modelName": anki_model,
                     "fields": {
                         "Name": taxon.name,
                         "Scientific Name": taxon.scientific_name,
@@ -94,12 +153,12 @@ def create_anki_card(taxon: Taxon):
                         "allowDuplicate": False,
                         "duplicateScope": "deck",
                         "duplicateScopeOptions": {
-                            "deckName": "Default",
+                            "deckName": anki_deck,
                             "checkChildren": False,
                             "checkAllModels": False,
                         },
                     },
-                    "tags": ["inaturalist"],
+                    "tags": ["inaturalist", *tags],
                     "picture": [
                         {
                             "url": image,
@@ -113,18 +172,30 @@ def create_anki_card(taxon: Taxon):
         },
     )
     resp.raise_for_status()
+    if error := resp.json()["error"]:
+        if "duplicate" not in error:
+            raise Exception(error)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("urls", nargs="+", description="taxa or search results page")
+    parser.add_argument("urls", nargs="+", help="taxa or search results page")
+    parser.add_argument("--anki-deck", required=True)
+    parser.add_argument("--anki-model", default="Inaturalist")
+    parser.add_argument("--anki-tags", nargs="+", default=[], help="anki tags")
     args = parser.parse_args()
 
-    for url in args.urls:
-        if "/taxa" in url:
-            taxon = download_taxon(url)
-            if taxon:
-                create_anki_card(taxon)
-        elif "/observations" in url:
-            for taxon in download_species_list(url):
-                create_anki_card(taxon)
+    create_anki_model(args.anki_model)
+
+    for url in tqdm.tqdm(args.urls, desc="url"):
+        try:
+            if "/taxa" in url:
+                taxon = download_taxon(url)
+                if taxon:
+                    create_anki_card(taxon, args.anki_deck, args.anki_model, args.tags)
+            elif "/observations" in url:
+                for taxon in tqdm(download_species_list(url), desc="species"):
+                    assert taxon  # tqdm seems to screw up type
+                    create_anki_card(taxon, args.anki_deck, args.anki_model, args.tags)
+        except Exception:
+            logging.warning("unable to process %s", url, exc_info=True)
